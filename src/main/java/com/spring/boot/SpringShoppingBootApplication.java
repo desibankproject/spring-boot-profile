5e4304852589b0dbad84637d17780bf97f928214
Synergistisitit <synergisticit2020@gmail.com>
2018-12-27 15:57:50 -0800

    first commit

---
+++ src/main/java/com/spring/boot/SpringShoppingBootApplication.java
@@ -0,0 +1,16 @@
+package com.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+//@SpringBootApplication(scanBasePackages={})
+@SpringBootApplication
+public class SpringShoppingBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringShoppingBootApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/spring/boot/springshoppingboot/SpringShoppingBootApplicationTests.java
@@ -0,0 +1,17 @@
+package com.spring.boot.springshoppingboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringShoppingBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
